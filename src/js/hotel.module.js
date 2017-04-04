39f086f60d3cc54baec7583b326695f595f64f6f
Jennifer Oakes <joakes10@gmail.com>
2017-04-04 18:10:08 -0400

    Created states for ui-router

--- src/js/hotel.module.js
+++ src/js/hotel.module.js
@@ -7,12 +7,15 @@
       routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
       function routerConfig ($stateProvider, $urlRouterProvider){
 
+        $urlRouterProvider.when('', '/');
+
+        $urlRouterProvider.otherwise('/not-found');
 
 
         $stateProvider
         .state({
             name: 'home',
-            url: '/home',
+            url: '/',
             templateUrl: 'views/home.template.html',
           })
           .state({
@@ -21,8 +24,37 @@
               templateUrl: 'views/login.template.html',
               controller: 'LoginController',
               controllerAs: 'loginCtrl'
+          })
+          .state({
+            name: 'about-us',
+            url: '/about-us',
+            templateUrl: 'views/about-us.template.html'
+          })
+          .state({
+            name: 'all-guests',
+            url: '/all-guests',
+            templateUrl: 'views/all-guests.template.html'
+          })
+          .state({
+            name: 'create-guest',
+            url: '/create-guest',
+            templateUrl: 'views/create-guest.template.html'
+          })
+          .state({
+            name: 'create-reservation',
+            url: '/create-reservation',
+            templateUrl: 'views/create-reservation.template.html'
+          })
+          .state({
+            name: 'reservations',
+            url: '/reservations',
+            templateUrl: 'views/reservations.template.html'
+          })
+          .state ({
+            name: 'not-found',
+            url: '/not-found',
+            templateUrl: 'views/not-found.template.html'
           });
-
 
       }
 
