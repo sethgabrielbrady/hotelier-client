cb6872dd0e542bc1c4f780d1bc8549d5272d9198
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-04-05 16:58:53 -0400

    staff service progress

---
+++ src/js/staff.service.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular.module('hotel')
+    .factory('StaffService', 'StaffService');
+
+  StaffService.$inject = ['$http'];
+
+  function StaffService($http) {
+
+  /**
+   * Add guest to 
+   */
+  function addGuest() {
+      return $http({
+
+      });
+  }
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+}());
