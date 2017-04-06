3eda4bbcc95f8ad652c11e070a4c82f7c257e9bf
Jordan Kasper <jordan@jordankasper.com>
2017-04-06 19:57:07 -0400

    Added basic controller and angular form functionality for entering a reservation ID to find

--- src/js/reservation.controller.js
+++ src/js/reservation.controller.js
@@ -2,9 +2,27 @@
   'use strict';
 
   angular.module('hotel')
-      .controller("ReservationController", ReservationController);
-      // console.log('Inside the controller');
+      .controller('ReservationController', ReservationController);
 
 
+  /**
+   * The ReservationController
+   */
+  function ReservationController() {
+    let vm = this;
+
+    vm.reservationInfo = {};
+
+    /**
+     * Send the user to the single reservation view when they enter an ID
+     * @param  {String}  id The ID of the reservation to send them to
+     * @return {Boolean}    Whether or not we sent them
+     */
+    vm.findReservation = function findReservation(id) {
+      console.log('Trying to find reservation:', id);
+      vm.reservationInfo = {};
+      return true;
+    };
+  }
 
 }());
