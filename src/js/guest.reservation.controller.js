68641c29b6744314109b0392055fd94f1a8880cc
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-14 17:16:21 -0400

    first hotelier commit. Changed some indentation on a controller

--- src/js/guest.reservation.controller.js
+++ src/js/guest.reservation.controller.js
@@ -1,40 +1,40 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('hotel')
-        .controller('GuestReservationController', GuestReservationController);
-    // console.log('Inside the controller');
+  angular.module('hotel')
+  .controller('GuestReservationController', GuestReservationController);
+  // console.log('Inside the controller');
 
 
-    GuestReservationController.$inject = ['$state', '$stateParams', 'ReservationService'];
+  GuestReservationController.$inject = ['$state', '$stateParams', 'ReservationService'];
 
-    /**
-     * Creates a log in controller so application can have miltiple users
-     * @param {Function} ReservationService this manages the reservation information
-     */
+  /**
+  * Creates a log in controller so application can have miltiple users
+  * @param {Function} ReservationService this manages the reservation information
+  */
 
-    function GuestReservationController($state, $stateParams, ReservationService) {
+  function GuestReservationController($state, $stateParams, ReservationService) {
 
-        let vm = this;
-        vm.reservationId = {};
-        vm.reservationInfo = null;
+    let vm = this;
+    vm.reservationId = {};
+    vm.reservationInfo = null;
 
-        vm.id = null;
-        vm.singleReservation = function(id) {
-            ReservationService.getGuestReservation(id).then(function handleSuccess(data) {
-                $state.go('single-reservation', {
-                    id: data
-                });
-            });
-        };
+    vm.id = null;
+    vm.singleReservation = function(id) {
+      ReservationService.getGuestReservation(id).then(function handleSuccess(data) {
+        $state.go('single-reservation', {
+          id: data
+        });
+      });
+    };
 
-        if ($stateParams.id) {
-            vm.reservationInfo = $stateParams.id;
-
-        }
-
+    if ($stateParams.id) {
+      vm.reservationInfo = $stateParams.id;
 
     }
+
+
+  }
 
 
 
